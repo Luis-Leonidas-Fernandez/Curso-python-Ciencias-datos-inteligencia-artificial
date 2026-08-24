f0b7960d0a17f1198ffef59e360df4cd29bd4de0
luis <fernandezluis303@gmail.com>
2026-08-24 20:53:53 -0300

    Operadores logicos

---
+++ clase_09.py
@@ -0,0 +1,27 @@
+a=10
+b=4
+print((2+3)* 4) #primero suma luego mueltiplica
+
+#* Operadores de comparacion (si o no - una sola vez)
+# a == b compara
+# a != b compara
+# a > b compara
+# a <= b compara
+edad_persona=17
+isTrue= True
+print(edad_persona <= 17)
+
+#* Comparadores logicos (si o no - varias veces)
+# and
+# or 
+# not
+
+
+print(not isTrue)
+
+nota= 8
+asistencia= 90
+
+print(nota > 7 and asistencia > 89)
+
+
